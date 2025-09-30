7f69be4000b7ab156ec56847d507470e223c32a7
rutvik patil <rutvik.patil@fourcolorstech.com>
2025-09-30 18:25:00 +0530

    ASE_Challenge

---
+++ prisma/migrations/20250929131530_new/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "Name" TEXT NOT NULL,
+    "Description" TEXT NOT NULL,
+    "stock_quntity" INTEGER NOT NULL,
+    "updateDate" DATETIME NOT NULL
+);
---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express=require("express");
+const productsRouter=require("./routes/productrouter");
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient();
+
+const app=express();
+const port =800;
+
+app.use(express.json());
+app.listen(port , ()=>{
+    console.log(`server is running on port ${port}`);
+})
+
+app.use("/api", productsRouter);
+
