16c39199a8ea5f466709a4790af1a10d7ed2cd81
rutvik patil <rutvik.patil@fourcolorstech.com>
2025-10-02 17:36:43 +0530

    Changes for sample html

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express=require("express");
 const productsRouter=require("./routes/productrouter");
 const { PrismaClient } = require('./generated/prisma');
 const prisma = new PrismaClient();
-
+const cors=require("cors");
 const app=express();
 const port =800;
 
+app.use(cors()); 
 app.use(express.json());
 app.listen(port , ()=>{
     console.log(`server is running on port ${port}`);
